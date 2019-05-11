4b755660eb44aee6ea34e5e12058deef9afb0dbb
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2019-05-11 23:16:17 +0700

    first try

---
+++ godogs_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DATA-DOG/godog"
+	"github.com/tebeka/selenium"
+)
+
+func openBukalapak() error {
+	caps := selenium.Capabilities{"browserName": "chrome"}
+	wd, err := selenium.NewRemote(caps, "")
+	if err != nil {
+		panic(err)
+	}
+	defer wd.Quit()
+
+	if err := wd.Get("https://www.bukalapak.com"); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func thereAreGodogs(available int) error {
+	Godogs = available
+	return nil
+}
+
+func iEat(num int) error {
+	if Godogs < num {
+		return fmt.Errorf("you cannot eat %d godogs, there are %d available", num, Godogs)
+	}
+	Godogs -= num
+	return nil
+}
+
+func thereShouldBeRemaining(remaining int) error {
+	if Godogs != remaining {
+		return fmt.Errorf("expected %d godogs to be remaining, but there is %d", remaining, Godogs)
+	}
+	return nil
+}
+
+func FeatureContext(s *godog.Suite) {
+	s.Step(`^there are (\d+) godogs$`, thereAreGodogs)
+	s.Step(`^I eat (\d+)$`, iEat)
+	s.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
+	s.Step(`^open browser$`, openBukalapak)
+
+	s.BeforeScenario(func(interface{}) {
+		Godogs = 0 // clean the state before every scenario
+	})
+}
